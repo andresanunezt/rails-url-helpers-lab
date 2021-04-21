faac0b92aa369554f5534cd8d219b59d68b3394d
Andrés Nuñez <andresanunez98@gmail.com>
2021-04-21 14:13:14 -0400

    commit tree 377fd74ef1e5c26d85e3d7d036f8ed40cc9ff4a1

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -4,6 +4,8 @@ class CreateStudents < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
 
+      t.boolean :active, default: false
+
       t.timestamps null: false
     end
   end
